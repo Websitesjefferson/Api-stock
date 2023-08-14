de2e5ea05036b513074cf8389faa23de47fb94f9
Jefferson <jeffersonneto1991@gmail.com>
2023-08-14 15:06:01 -0300

    initial commit

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import path from 'path'
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
